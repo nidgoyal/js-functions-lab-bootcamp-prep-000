726d632696ba4fc075d95063fa7b076d544e90eb
Nidhi <nidgoyal1@gmail.com>
2017-12-04 02:35:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  console.log(`Happy holidays, ${name}!`)
+  console.log(happyHolidays(), ${name}!`)
 }
 
 function happyHolidayTo(holiday, name){
